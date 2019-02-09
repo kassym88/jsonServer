c51fda73adf608bf3ff564797f0480bc8d75fc20
kassym88 <kassym88@gmail.com>
2019-02-09 13:56:36 +0600

    init

---
+++ services/core/auth/auth.middleware.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const url = require('url');
+
+module.exports = (server) => {
+
+	router.post('/auth/login', (req, res, next) => {
+		/*req.on('data', function (data) {
+			const body = JSON.parse(data.toString());
+
+
+			   console.log(body);
+			   let users = server.db.getState().users,
+		   matchedUser = users.find((user) => {
+			   const ret = user.login.toUpperCase() === body.login.toUpperCase();
+			   if(ret)console.log(user);
+			   return ret;
+		   });
+
+		   if(!matchedUser) {
+			   res.status(401).send('Wrong username');
+		   } else if(matchedUser.password === body.password) {
+			   res.json({ token: matchedUser.fakeToken});
+		   } else {
+			   res.status(401).send("Wrong password");
+		   }
+		});*/
+		console.log('body', req.body);
+		const login = req.body.data.login,
+			password = req.body.data.password;
+		console.log('login', login);
+		console.log('password', password);
+		if(login && password){
+            const searchUser = server.db.getState().users.find(user =>
+            	user.login.toUpperCase() === login.toUpperCase() && user.password === password
+			);
+            if(searchUser)
+            	res.json({token: searchUser.fakeToken});
+            else
+            	res.status(401).send('User not found');
+		}
+		else
+			res.status(401).send('Login or/and password not provided');
+	});
+		
+	router.post('/auth/userinfo', (req, res, next) => {
+		let users = server.db.getState().users,
+			matchedUser = users.find((user) => {
+				console.log(user);
+				return user.fakeToken === req.header('Authorization');
+			});
+
+		if(!matchedUser) {
+			res.status(401).send('Unauthorized');
+		} else {
+			res.json(matchedUser);
+		}
+	});
+
+	return router;
+};
